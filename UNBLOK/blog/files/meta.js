a683e9252bd816a5c105cd6cba44e841d3de04c3
surya133007 <surya133007@gmail.com>
2020-11-16 18:31:35 +0530

    initial commit

---
+++ UNBLOK/blog/files/meta.js
@@ -0,0 +1,74 @@
+(function() {
+	var Realmac = Realmac || {};
+
+	Realmac.meta = {
+		
+		// Set the browser title
+		//
+		// @var String text
+		setTitle: function(text) {
+			return document.title = text;
+		},
+		
+		// Set the content attribute of a meta tag
+		//
+		// @var String name
+		// @var String content
+		setTagContent: function(tag, content){
+			// If the tag being set is title
+			// return the result of setTitle
+			if ( tag === 'title' )
+			{
+				return this.setTitle(content);
+			}
+			
+			// Otherwise try and find the meta tag
+			var tag = this.getTag(tag);
+			
+			// If we have a tag, set the content
+			if ( tag !== false )
+			{
+				return tag.setAttribute('content', content);
+			}
+			
+			return false;
+		},
+		
+		// Find a meta tag
+		//
+		// @var String name
+		getTag: function(name) {
+			var meta = document.querySelectorAll('meta');
+			
+			for ( var i=0; i<meta.length; i++ )
+			{
+				if (meta[i].name == name){
+					return meta[i];
+				}
+			}
+			
+			var tag = document.createElement('meta');
+			tag.name = name;
+			document.getElementsByTagName('head')[0].appendChild(tag);
+			
+			return tag;
+		}
+	};
+ 
+	// Object containing all website meta info
+	var websiteMeta = {"archive-november-2020.html":"Archives for November 2020","26b59fd1d8d125bf9600f010b83d1321-2.html":"PHOTOSHOP Proficiency, ADOBE ILLUSTRATOR, Good in English language and a super Creative person who is always looking to learn more. Should be very muc","5dded2f75fe34ced47baee4d162969d3-1.html":"A Full Stack Leader with at-least 2 years of experience.\n\nAPPLY BY EMAIL","afc457df2199de48485f957558062751-0.html":"\t•\tMinimum 3 yrs of experience in NodeJS\n\t•\tExcellent TEAM Handling Experience\n\t•\tAt least 3 years of experience in the capacity of a tech lead\/archit"};
+ 
+	// pageId must match the key in websiteMeta object
+	var url = window.location.pathname;
+	var pageId = url.substring(url.lastIndexOf('/')+1);
+	if (!pageId || pageId.length == 0){
+		pageId = 'index.html';
+	}
+	pageMeta = websiteMeta[pageId];
+ 
+	// If we have meta for this page
+	if (pageMeta){
+		Realmac.meta.setTagContent('description', pageMeta);
+	}
+ 
+ })();
